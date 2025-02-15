9ec768344470abeae49f0e0c32614f4f9445640e
Dona Eric KOULODJI <donaerickoulodji@gmail.com>
2025-02-15 02:47:07 +0100

    Update urls.py service

--- Data_Portfolio/port_data/urls.py
+++ Data_Portfolio/port_data/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     #path('skills/', views.skills, name = 'skills'),
     path('projects/', views.projects, name="projects"),
     path('contacts-us/', views.contacts, name='contacts-us'),
-    path('services/', views.services, name ="services"),
-    path('services_request/<int:id_service>/', views.services_request, name ="service_request_with_id"),
-    path('services_request/', views.services_request, name = 'services_request'),
+   # path('services/', views.services, name ="services"),
+    #path('services_request/<int:id_service>/', views.services_request, name ="service_request_with_id"),
+   # path('services_request/', views.services_request, name = 'services_request'),
     path('blog/', views.article, name='blog'),
-    path('news/', views.newsletters, name = 'news')
-
+    path('news/', views.newsletters, name = 'news'),
+    path('service/', views.service_list, name='service'),
+    path('service/<int:pk>/', views.service_detail, name='service_detail')
 ]
